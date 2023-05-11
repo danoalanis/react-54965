a48528036444670fb3f0080e1120473f37a89541
Daniel Alanis <133169585+danoalanis@users.noreply.github.com>
2023-05-10 22:34:57 -0300

    Fixing app error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const myFunction = () => {
+    console.log("Hola a todos")
+  };
+  
   return (
     <div className="App">
-      
+      <button onClick={myFunction}>Hola Mundo</button>
     </div>
   );
 }
